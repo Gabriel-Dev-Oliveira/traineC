a7299e799ac4e918371e5844713d3a3cd95ecbed
Gabriel de Oliveira Rocha <97638988+Gabriel-Dev-Oliveira@users.noreply.github.com>
2023-05-16 21:40:11 -0300

    Update loop_array.c

--- loop_array.c
+++ loop_array.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-int main() {
-float notas[4];
+int main()
+{
+    float notas[4];
 
-for(int i=0; i<4; i++){
-printf("Digite a nota:");
-scanf("%f", &notas[i]);
-}
+    for(int i=0; i<4; i++)
+    {
+        printf("\nDigite a nota:");
+        scanf("%f", &notas[i]);
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\na nota é: %.2f", notas[i]);
+    }
     return 0;
 }
